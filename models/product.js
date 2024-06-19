8b8b0b77ba2e6c92e93f274a2b793230918fceac
SavaniDhruv610 <dhruv.savani610@gmail.com>
2024-06-19 17:58:34 +0530

    19th june-2

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
@@ -16,6 +17,11 @@ const productSchema = new Schema({
   },
   imageUrl: {
     type: String,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
     required: true
   }
 });
