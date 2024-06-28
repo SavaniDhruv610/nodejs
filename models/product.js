97c0e86dc714f6b49ef5aa63677e36329de3824d
SavaniDhruv610 <dhruv.savani610@gmail.com>
2024-06-28 15:56:39 +0530

    28th june

--- models/product.js
+++ models/product.js
@@ -15,10 +15,12 @@ const productSchema = new Schema({
     type: String,
     required: true,
   },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
+  imageUrl: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
   userId: {
     type: Schema.Types.ObjectId,
     ref: "User",
